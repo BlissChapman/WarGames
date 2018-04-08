a41e2cb1978f90ec1969fdcd285249656e2fb582
varsha804 <varsha2@illinois.edu>
2018-04-08 13:17:20 -0500

    Modified original code so that it supports 2 agents.

--- src/environments/GridWorld.py
+++ src/environments/GridWorld.py
@@ -7,6 +7,7 @@ class GridWorld:
         self.width = 5
         self.height = 5
         self.reset()
+        self.obstacles = []
 
     def reset(self):
         self.agent_x = 0
@@ -15,7 +16,7 @@ class GridWorld:
         self.goal_y = self.height - 1
         self.goal_x = self.width - 1
 
-    def render(self, output_file):
+    def render(self, output_file, add_obstacle):
         # Set up background tile checkerboard pattern
         img = np.empty(self.height*self.width)
         img[::2] = 1.0
@@ -25,6 +26,14 @@ class GridWorld:
         # Mark goal and agent
         img[self.goal_y, self.goal_x] = 0.4
         img[self.agent_y, self.agent_x] = 0.2
+
+        if add_obstacle:
+            for i in range(self.height-2):
+                img[i, 1] = 0.7
+                obstacles.append(tuple(i, 1))
+            for i in range(1, self.width):
+                img[self.height-3, i] = 0.7
+                obstacles.append(tuple(self.height-3, i))
 
         # Save to output file
         figure = plt.figure()
--- src/games/ExploreGame.py
+++ src/games/ExploreGame.py
@@ -2,32 +2,51 @@ from environments.GridWorld import GridWorld
 
 class ExploreGame:
 
-    def __init__(self, agent):
-        self.agent = agent
+    def __init__(self, agent1, agent2=None):
+        self.agent1 = agent1
+        self.agent2 = agent2
         self.env = GridWorld()
         self.num_steps_in_game = 25
 
     def reset(self):
         self.env.reset()
 
-    def step(self):
+    def step(self, agent_num):
         # Build state representation of the current environment
         state_tuple = ExploreGame.state_representation(self.env)
 
         # Retrieve agent's chosen action for state
         action_choices = ['N', 'E', 'S', 'W', '_']
-        action_choice = self.agent.action(state=state_tuple,
+        if agent_num == 1:
+            action_choice = self.agent1.action(state=state_tuple,
                                           action_choices=action_choices)
-
+        else:
+            action_choice = self.agent2.action(state=state_tuple,
+                                          action_choices=action_choices)
         # Simulate effect of agent's chosen action
         if action_choice == 'N':
-            self.env.agent_y = max(self.env.agent_y - 1, 0)
+            if tuple((self.env.agent_x, self.env.agent_y - 1)) in self.env.obstacles:
+                print("Agent attempted action '{0}' that is blocked by an obstacle.".format(action_choice))
+            else:
+                self.env.agent_y = max(self.env.agent_y - 1, 0)
         elif action_choice == 'E':
-            self.env.agent_x = min(self.env.agent_x + 1, self.env.width - 1)
+            if tuple((self.env.agent_x + 1, self.env.agent_y)) in self.env.obstacles:
+                print("Agent attempted action '{0}' that is blocked by an obstacle.".format(action_choice))
+            else:
+                self.env.agent_x = min(self.env.agent_x + 1, self.env.width - 1)
+            
         elif action_choice == 'S':
-            self.env.agent_y = min(self.env.agent_y + 1, self.env.height-1)
+            if tuple((self.env.agent_x, self.env.agent_y + 1)) in self.env.obstacles:
+                print("Agent attempted action '{0}' that is blocked by an obstacle.".format(action_choice))
+            else:
+                self.env.agent_y = min(self.env.agent_y + 1, self.env.height-1)
+            
         elif action_choice == 'W':
-            self.env.agent_x = max(self.env.agent_x - 1, 0)
+            if tuple((self.env.agent_x - 1, self.env.agent_y)) in self.env.obstacles:
+                print("Agent attempted action '{0}' that is blocked by an obstacle.".format(action_choice))
+            else:
+                self.env.agent_x = max(self.env.agent_x - 1, 0)
+            
         elif action_choice == '_':
             pass
         else:
@@ -38,7 +57,10 @@ class ExploreGame:
 
         # Allow agent to learn
         new_state_tuple = ExploreGame.state_representation(self.env)
-        self.agent.learn(state_tuple, action_choice, new_state_tuple, reward)
+        if agent_num == 1:
+            self.agent1.learn(state_tuple, action_choice, new_state_tuple, reward)
+        else:
+            self.agent2.learn(state_tuple, action_choice, new_state_tuple, reward)
 
     def state_representation(env):
         return (env.agent_x, env.agent_y)
