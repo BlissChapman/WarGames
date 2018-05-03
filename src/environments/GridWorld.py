7b74a4036a493c4431fcfa7b3436bc1c6f178220
Varsha Subrahmanyam <varsha2@illinois.edu>
2018-05-03 17:16:15 -0500

    Update GridWorld.py

--- src/environments/GridWorld.py
+++ src/environments/GridWorld.py
@@ -9,10 +9,6 @@ class GridWorld:
         
         self.agents_x = [0, 5]
         self.agents_y = [5, 0]
-        self.obstacles = []
-
-        self.one_move = True
-        self.two_move = True
 
         self.reset()
 
@@ -20,13 +16,7 @@ class GridWorld:
         self.agents_x = [0, 5]
         self.agents_y = [5, 0]
 
-        self.goal_y = self.height - 1
-        self.goal_x = self.width - 1
-
-        self.one_move = True
-        self.two_move = True
-
-    def render(self, output_file, add_obstacle):
+    def render(self, output_file):
         # Set up background tile checkerboard pattern
         self.img = np.zeros((self.height, self.width))
         self.img[::, ::] = 1.0
@@ -36,23 +26,13 @@ class GridWorld:
 
         colors = [0.2, 0.05, 0.6]
 
-        # Mark goal and agent
-        self.img[self.goal_y, self.goal_x] = 0.4
-
+        # Mark and agents
         for i in range(len(self.agents_x)):
             self.img[self.agents_y[i], self.agents_x[i]] = colors[i]
         if not self.one_move:
             self.img[self.agents_y[0], self.agents_x[0]] = 0.8
         if not self.two_move:
             self.img[self.agents_y[1], self.agents_x[1]] = 0.8
-
-        if add_obstacle:
-            for i in range(self.height-2):
-                self.img[i, 2] = 0.7
-                self.obstacles.append((2, i))
-            for i in range(2, self.width):
-                self.img[self.height-2, i] = 0.7
-                self.obstacles.append((i, self.height-2))
 
         # Save to output file
         figure = plt.figure()
