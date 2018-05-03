c480bbf5fc097079aeccc1508d5492ebcade2a99
Varsha Subrahmanyam <varsha2@illinois.edu>
2018-05-03 17:08:05 -0500

    Update ExploreGame.py

--- src/games/ExploreGame.py
+++ src/games/ExploreGame.py
@@ -2,47 +2,69 @@ from environments.GridWorld import GridWorld
 
 class ExploreGame:
 
-    def __init__(self, agent):
-        self.agent = agent
-        self.env = GridWorld()
+    def __init__(self, agent1, agent2):
+        self.agent1 = agent1
+        self.agent2 = agent2
+
+        self.env = GridWorld(2)
+
         self.num_steps_in_game = 25
 
     def reset(self):
         self.env.reset()
 
-    def step(self):
+    def step(self, num):
         # Build state representation of the current environment
         state_tuple = ExploreGame.state_representation(self.env)
 
+
         # Retrieve agent's chosen action for state
-        action_choices = ['N', 'E', 'S', 'W', '_']
-        action_choice = self.agent.action(state=state_tuple,
+        action_choices = ['N', 'E', 'S', 'W']
+        if num == 0:
+            action_choice = self.agent1.action(state=state_tuple,
                                           action_choices=action_choices)
+            last_pos = self.agent1.last_pos = (self.env.agents_x[num], self.env.agents_y[num])
+        else:
+            action_choice = self.agent2.action(state=state_tuple,
+                                          action_choices=action_choices)
+            last_pos = self.agent2.last_pos = (self.env.agents_x[num], self.env.agents_y[num])
 
         # Simulate effect of agent's chosen action
         if action_choice == 'N':
-            self.env.agent_y = max(self.env.agent_y - 1, 0)
+            print("N")
+            self.env.agents_y[num] = max(self.env.agents_y[num] - 1, 0)
         elif action_choice == 'E':
-            self.env.agent_x = min(self.env.agent_x + 1, self.env.width - 1)
+            print("E")
+            self.env.agents_x[num] = min(self.env.agents_x[num] + 1, self.env.width - 1)
         elif action_choice == 'S':
-            self.env.agent_y = min(self.env.agent_y + 1, self.env.height-1)
+            print("S")
+            self.env.agents_y[num] = min(self.env.agents_y[num] + 1, self.env.height-1)
         elif action_choice == 'W':
-            self.env.agent_x = max(self.env.agent_x - 1, 0)
+            print("W")
+            self.env.agents_x[num] = max(self.env.agents_x[num] - 1, 0)
         elif action_choice == '_':
+            print("_")
             pass
         else:
             print("Agent attempted action '{0}' that is unavailable in ExploreGame.".format(action_choice))
-
         # Compute reward
-        reward = ExploreGame.reward(self.env)
+        reward = ExploreGame.reward(self.env, num, last_pos)
 
         # Allow agent to learn
         new_state_tuple = ExploreGame.state_representation(self.env)
-        self.agent.learn(state_tuple, action_choice, new_state_tuple, reward)
+        if num == 0:
+            self.agent1.learn(state_tuple, action_choice, new_state_tuple, reward)
+        else:
+            self.agent2.learn(state_tuple, action_choice, new_state_tuple, reward)
 
     def state_representation(env):
-        return (env.agent_x, env.agent_y)
+        return (env.agents_x[0], env.agents_y[0], env.agents_x[1], env.agents_y[1])
 
-    def reward(env):
-        agentReachedGoalState = (env.agent_x, env.agent_y) == (env.goal_x, env.goal_y)
-        return 1.0 if agentReachedGoalState else -0.1
+    def reward(env, num, last_pos):
+        mirroring = ((env.agents_y[0] == env.agents_y[1]) and (env.agents_x[0] + env.agents_x[1] == 5) and (env.agents_x[0] != env.agents_x[1]))
+        not_moving = last_pos == (env.agents_x[num], env.agents_y[num])
+        if not_moving:
+            return -0.1
+        elif mirroring:
+            return 2.0
+        return -0.1
